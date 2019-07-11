7a80ab2f78f3b54a46d24260563e3292c8ab1100
hexi <xi.he@baidao.com>
2019-07-12 00:00:18 +0800

    Update LifecycleCoroutineScope to use SupervisorJob

--- app/src/main/java/com/hexi/kotlindemo/coroutineSupport/LifecycleCoroutineScope.kt
+++ app/src/main/java/com/hexi/kotlindemo/coroutineSupport/LifecycleCoroutineScope.kt
@@ -2,13 +2,13 @@ package com.hexi.kotlindemo.coroutineSupport
 
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
+import kotlinx.coroutines.SupervisorJob
 import java.io.Closeable
 import kotlin.coroutines.CoroutineContext
 
 class LifecycleCoroutineScope : CoroutineScope, Closeable {
 
-    private val job = Job()
+    private val job = SupervisorJob()
     override val coroutineContext: CoroutineContext
         get() = Dispatchers.Main + job
 
